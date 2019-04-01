492ac39365fc143ca89722d369b5c3a7429c3353
ADN <1434-3541@mail.ru>
2019-04-01 15:09:47 +0400

    Commit 1

---
+++ src/test/java/MyStreamTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+public class MyStreamTest {
+
+    @Test
+    public void of() {
+    }
+
+    @Test
+    public void filter() {
+    }
+
+    @Test
+    public void transform() {
+    }
+
+    @Test
+    public void toMap() {
+    }
+}
